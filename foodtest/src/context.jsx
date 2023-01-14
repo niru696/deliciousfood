dea76ae074a3fb8a55175f5e8d92ff2f099deb68
niranjan kumar choudhary <nkchoudhary696@gmail.com>
2023-01-14 10:06:08 +0530

    Update PR

---
+++ foodtest/src/context.jsx
@@ -0,0 +1,54 @@
+import React, {useContext,useEffect, useState} from "react";
+import axios from 'axios'
+
+const AppContext =  React.createContext();
+const allMealUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
+const randomMealURL = 'https://www.themealdb.com/api/json/v1/1/random.php'
+
+
+const AppProvider = ({children}) => {
+    const [loading, setLoading] = useState(false);
+    const [meals, setMeals] = useState([]);
+    const [searchtext, setSearchText] = useState('');
+    const [Mealscount, setMealscount] = useState(0);
+    const fetRandomMeals = () => {
+        fetchMeals(randomMealURL);
+    }
+    const refreshPage = () =>{
+        fetchMeals(allMealUrl);
+    }
+    const fetchMeals = async (url) => {
+        setLoading(true);
+        try{
+         const {data} = await axios(url);
+         if(data.meals){
+            setMealscount(data.meals.length)
+            setMeals(data.meals);
+         }else{
+            setMeals([]);
+         }
+         
+        }
+        catch(error){
+           console.log(error.response);
+        }
+        setLoading(false)
+        }
+        useEffect(() =>{
+            fetchMeals(allMealUrl);
+        },[])    
+    useEffect(() =>{
+        if(!searchtext) return
+        fetchMeals(`${allMealUrl}${searchtext}`);
+    },[searchtext])
+    return(
+        <AppContext.Provider value={{meals, loading,setSearchText, fetRandomMeals,refreshPage,Mealscount}}>
+           {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useGlobalContex = () => {
+    return useContext(AppContext);
+}
+export {AppContext, AppProvider}
